5e1f6f9f847fb6405c78309d2be9d50a594273f7
Jonas Lobe <jonas.lobe.2006@outlook.com>
2021-12-06 20:12:03 +0100

    Initial commit

---
+++ src/main/java/de/jonas/CouponGenerator.java
@@ -0,0 +1,11 @@
+package de.jonas;
+
+import org.jetbrains.annotations.NotNull;
+
+public final class CouponGenerator {
+
+    public static void main(@NotNull final String @NotNull [] args) {
+
+    }
+
+}
